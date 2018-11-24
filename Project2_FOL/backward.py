161c742b71857e950106099af3f112d02ffeb020
bao <baosws@gmail.com>
2018-11-25 02:26:10 +0700

    its 2.30AM

--- Project2_FOL/backward.py
+++ Project2_FOL/backward.py
@@ -17,10 +17,13 @@ def getArgs(rule):
     args = rule[rule.index('(') + 1:].replace(')', '').replace(',', ' ').split()
     return args
 
-def getPredNVars(pred):
+def getName(pred):
     ls = pred.replace('(', ' ').replace(')', ' ').replace(',', ' ').split()
-    return ls[0], [var for var in ls[1:] if isVariable(var)]
+    return ls[0]
 
+def getVars(pred):
+    ls = pred.replace('(', ' ').replace(')', ' ').replace(',', ' ').split()
+    return [var for var in ls[1:] if isVariable(var)]
 
 def getPreds(rule):
     rule = rule.replace('.', '').replace(' ', '').split(':-')[1]
@@ -34,7 +37,7 @@ def getPreds(rule):
 def isVariable(s):
     return s[0].isupper() or s[0] == '_'
 
-def unifiable(pred, rule):
+def delegable(pred, rule):
     if pred.name == rule.name\
             and len(pred.args) == len(rule.args)\
             and all((isVariable(pa) and isVariable(ra)) or (not isVariable(pa) and not isVariable(ra) and pa == ra) or (isVariable(ra) and not isVariable(pa))\
@@ -42,11 +45,10 @@ def unifiable(pred, rule):
         return True
     return False
  
-def getID(pred):
-    args = pred.args[:]
+def getNormId(pred):
     c = 0
     id = [pred.name]
-    for arg in args:
+    for arg in pred.args:
         if isVariable(arg):
             id.append(f'X{c}')
             c += 1
@@ -55,47 +57,37 @@ def getID(pred):
     return tuple(id)
 
 import copy
-def newPredWithLessVars(pred, sub_map):
-    newPred = copy.deepcopy(pred)
-    newPred.args = [sub_map.get(arg, arg) for arg in pred.args]
-    newPred.vars = [arg for arg in newPred.args if isVariable(arg)]
-    newPred.unifiable_rules = [rule for rule in uni_rules.values() if unifiable(newPred, rule)]
-    
-    return newPred
     
 def newRuleWithLessVars(pred, rule):
-    newRule = copy.deepcopy(rule)
+    new_rule = copy.deepcopy(rule)
     sub_map = {x:y for x, y in zip(rule.args, pred.args) if isVariable(x) and not isVariable(y)}
-    newRule.args = [sub_map.get(arg, arg) for arg in rule.args]
-    newRule.preds = [newPredWithLessVars(pred, sub_map) for pred in newRule.preds]
-    newRule.vars = rule.vars - set(sub_map.keys())
-    newRule.arg_vars = set(newRule.args) & set(newRule.vars)
-    id = getID(newRule)
+    new_rule.args = [sub_map.get(arg, arg) for arg in rule.args]
+    new_rule.preds = [pred.sub(sub_map) for pred in new_rule.preds]
+    new_rule.vars = rule.vars - set(sub_map.keys())
+    new_rule.arg_vars = set(new_rule.args) & set(new_rule.vars)
+    id = getNormId(new_rule)
     if id not in uni_rules:
-        uni_rules[id] = newRule
+        uni_rules[id] = new_rule
     else:
         return uni_rules[id]
+
     for pred in uni_preds:
-        if unifiable(pred, newRule):
-            pred.unifiable_rules.append(newRule)
+        if delegable(pred, new_rule):
+            pred.delegable_rules.append(new_rule)
     
-#     print('newRuleWithLessVars', pred, rule, newRule, sep='\n')
-#     exit(0)
-    return newRule
+    return new_rule
     
 class Rule:
-    # args: list of PlaceHolder
-    # eval string
     def __init__(self, rule):
         self.name = getName(rule)
         self.preds = getPreds(rule)
         self.args = getArgs(rule)
         self.vars = set(var for var in self.args + list(itertools.chain(*[pred.vars for pred in self.preds])) if isVariable(var))
         self.arg_vars = set(self.args) & set(self.vars)
-        uni_rules[getID(self)] = self
+        uni_rules[getNormId(self)] = self
         for pred in uni_preds:
-            if unifiable(pred, self):
-                pred.unifiable_rules.append(self)
+            if delegable(pred, self):
+                pred.delegable_rules.append(self)
 
     def __repr__(self):
         return f'<RULE {self.name} {self.args} {self.vars} {self.preds}>'
@@ -103,12 +95,11 @@ class Rule:
     # return set of tuples of subtitutes
     def getSubsInFacts(self):
         subs = set()
-        for sub in itertools.product(universe, repeat = len(self.arg_vars)):
-            sub_map = {x: y for x, y in zip(self.arg_vars, sub)}
+        for sub in itertools.product(universe, repeat = len(self.vars)):
+            sub_map = {x: y for x, y in zip(self.vars, sub)}
             v = tuple(sub_map.get(arg, arg) for arg in self.args)
             if v in uni_facts[self.name]:
                 subs.add(v)
-#         print('RULE', 'getSubsInFact', self, subs)
         return subs
 
     def check(self, sub_map):
@@ -118,20 +109,17 @@ class Rule:
     def getSubsByPreds(self):
         pred_subs = [pred.getSubs() for pred in self.preds]
 
-#         print('>> preds', self, self.preds)
         subs = set()
         for pred_sub in itertools.product(*pred_subs):
             var_subs = set(list(itertools.chain(*[[(x, y) for x, y in zip(pred.args, sub) if isVariable(x)] for pred, sub in zip(self.preds, pred_sub)])))
-#             print('>> varsubs', self, var_subs)
             if len(var_subs) == len(self.vars):
                 sub_map = {x:y for x, y in var_subs}
                 subs.add(tuple(sub_map.get(arg, arg) for arg in self.args))
-#         print('>> RULE getSubsByPreds', self, subs)
             
         return subs
 
     def getSubs(self):
-        id = getID(self)
+        id = getNormId(self)
         if id not in uni_subs:
             uni_subs[id] = self.getSubsInFacts() | self.getSubsByPreds()
 #             print('>> RULE getSubs', id, uni_subs[id])
@@ -139,15 +127,25 @@ class Rule:
 
 class Predicate:
     def __init__(self, pred):
-        self.name, self.vars = getPredNVars(pred)
+        self.name = getName(pred)
+        self.vars = getVars(pred)
         self.vars = set(self.vars)
         self.args = getArgs(pred)
-        self.unifiable_rules = [rule for rule in uni_rules.values() if unifiable(self, rule)]
         uni_preds.append(self)
+        self.delegable_rules = [rule for rule in uni_rules.values() if delegable(self, rule)]
 
     def __repr__(self):
         return f'<PRED {self.name} {self.args}>'
     
+    def sub(self, sub_map):
+        pred = copy.deepcopy(self)
+        pred.args = [sub_map.get(arg, arg) for arg in self.args]
+        pred.vars = [arg for arg in pred.args if isVariable(arg)]
+        uni_preds.append(pred)
+        pred.delegable_rules = [rule for rule in uni_rules.values() if delegable(pred, rule)]
+    
+        return pred
+
     def check(self, subs):
         return tuple(subs.get(arg, arg) for arg in self.args) in self.getSubs()
 
@@ -158,20 +156,18 @@ class Predicate:
             v = tuple(sub_map.get(arg, arg) for arg in self.args)
             if v in uni_facts[self.name]:
                 subs.add(v)
-#         print('PRED', 'getSubsInFact', self.name, subs)
         return subs
         
     def getSubsInRules(self):
         subs = set()
-        for rule in self.unifiable_rules:
+        for rule in self.delegable_rules:
             subs.update(newRuleWithLessVars(self, rule).getSubs())
         return subs
 
     def getSubs(self):
-        id = getID(self)
+        id = getNormId(self)
         if id not in uni_subs:
             uni_subs[id] = self.getSubsInFacts() | self.getSubsInRules()
-#             print('>> PRED getSubs', id, uni_subs[id], '\n')
         return uni_subs[id]
 
 class Fact:
@@ -180,7 +176,6 @@ class Fact:
         self.args = getArgs(fact)
         self.vars = set(arg for arg in self.args if isVariable(arg))
         self.insts = set(arg for arg in self.args if not isVariable(arg))
-#         print(self.name, self.insts, self.vars)
     
     def activate(self):
         for sub in itertools.product(universe, repeat = len(self.vars)):
@@ -188,15 +183,15 @@ class Fact:
             v = tuple(sub_map.get(arg, arg) for arg in self.args)
             uni_facts[self.name].add(v)
 
-class Question:
-    def __init__(self, question):
-        self.pred = Predicate(question)
-        self.pred.unifiable_rules = [rule for rule in uni_rules.values() if unifiable(self.pred, rule)]
+class Question(Rule):
+    def __init__(self, content):
+        q = 'q():-' + content
+        q = str(id(q)) + '(' + ','.join(Rule(q).vars) + '):-' + content
+        self.cont = Rule(q)
 
     def getAns(self):
-        return list(map(lambda sub: {x:y for x, y in zip(self.pred.args, sub) if isVariable(x)}, self.pred.getSubs()))
+        return list(map(lambda sub: {x:y for x, y in zip(self.cont.vars, sub) if isVariable(x)}, self.cont.getSubs()))
     
-           
 facts = []
 def readKB():
     with open('script.pl') as fin:
