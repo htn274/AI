f26a87941089d3e6de06b2d81e661e305d22da38
bao <baosws@gmail.com>
2018-11-25 21:08:19 +0700

    fixed forward_new

---
+++ Project2_FOL/food.pl
@@ -0,0 +1,83 @@
+person(bao).
+person(nu).
+person(cuong).
+person(duy).
+
+cooker(bao).
+
+ingredient(salt).
+ingredient(sugar).
+ingredient(pepper).
+ingredient(soy_sauce).
+ingredient(fish_sauce).
+ingredient(cooking_oil).
+ingredient(butter).
+ingredient(vinegar).
+
+ingredient(garlic).
+ingredient(onion).
+ingredient(chili).
+ingredient(chiken).
+ingredient(fish).
+ingredient(pork).
+ingredient(water).
+ingredient(coconut).
+ingredient(egg).
+ingredient(ginger).
+ingredient(peanut).
+ingredient(tomato).
+ingredient(potato).
+ingredient(wine).
+ingredient(flour).
+
+dish(fried_egg).
+dish(fried_fish).
+dish(poached_fish).
+dish(poached_pork_and_egg).
+dish(cake).
+
+event(birthday).
+event(wedding).
+event(family).
+event(
+contains(fried_egg, egg).
+contains(fried_egg, salt).
+contains(fried_egg, pepper).
+contains(fried_egg, butter).
+contains(fried_egg, fish_sauce).
+contains(fried_egg, tomato).
+
+contains(poached_fish, fish).
+contains(poached_fish, fish_sauce).
+contains(poached_fish, onion).
+contains(poached_fish, pepper).
+contains(poached_fish, water).
+contains(poached_fish, sugar).
+
+contains(poached_pork_and_egg, pork).
+contains(poached_pork_and_egg, egg).
+contains(poached_pork_and_egg, water).
+contains(poached_pork_and_egg, soy_sauce).
+contains(poached_pork_and_egg, fish_sauce).
+contains(poached_pork_and_egg, sugar).
+contains(poached_pork_and_egg, pepper).
+contains(poached_pork_and_egg, coconut).
+contains(poached_pork_and_egg, onion).
+contains(poached_pork_and_egg, ginger).
+contains(poached_pork_and_egg, vinegar).
+contains(poached_pork_and_egg, wine).
+contains(poached_pork_and_egg, cooking_oil).
+
+contains(fried_fish, fish).
+contains(fried_fish, cooking_oil).
+contains(fried_fish, garlic).
+contains(fried_fish, fish_sauce).
+contains(fried_fish, vinegar).
+contains(fried_fish, chili).
+contains(fried_fish, pepper).
+
+contains(cake, flour).
+contains(cake, egg).
+contains(cake, water).
+contains(cake, sugar).
+contains(cacke, milk).
